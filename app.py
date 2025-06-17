e8514ed5396119f3f5c922cacf3e9f2690268a2f
jazminhurtado <jazhurgraster@gmail.com>
2025-06-16 22:28:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+# Esto hace que app.py no muestre nada como página
+import os
 import streamlit as st
+
+# Oculta todos los elementos visuales por si acaso
+st.set_page_config(page_title="Redirigiendo...", layout="wide")
+st.markdown("<style>main, header, footer {display: none !important;}</style>", unsafe_allow_html=True)
+
+# Redirige silenciosamente
 st.switch_page("pages/0_Inicio.py")
+
 
 
