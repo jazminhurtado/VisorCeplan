3459a156ed71cfed302e049dd23804303d4a481f
jazminhurtado <jazhurgraster@gmail.com>
2025-06-17 08:56:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import streamlit as st
 st.set_page_config(page_title="Visor CEPLAN", page_icon="📊", layout="wide")
 #st.markdown("# 🏠 Inicio")
 
-st.title("📊 Bienvenido al visor CEPLAN: consulta Políticas Nacionales, PDC, PEI y POI fácilmente")
+st.title("📊 Bienvenido al visor CEPLAN: Consulta Políticas Nacionales, PDC, PEI y POI fácilmente")
 
 st.markdown("""
 ### Bienvenido al sistema unificado de visores
 
 Desde aquí puedes consultar:
-- 📋 PEI, POI y PDC por Unidad Ejecutora
+- 📋 PDC, PEI y POI por Unidad Ejecutora
 - 🏛️ Políticas Nacionales aprobadas
 
 Utiliza el menú lateral para navegar entre los visores.
